f7edf3b6f85d339faf30129ac061e0de9d12e89f
HarryharricChen <56270842+HarryharricChen@users.noreply.github.com>
2019-11-16 22:23:32 -0800

    Add Moving Script

---
+++ CollaborationHarryRosaita/Assets/Scripts/playerMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMove : MonoBehaviour
+{
+    Rigidbody rb;
+    public float playerSpeed = 5f;
+    Vector3 moveDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        moveDirection = new Vector3(horizontalInput * playerSpeed, -9.8f, 0);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = moveDirection;
+    }
+}
